244e91b21d1ce732dc2ae9409e44ddc48d0568c1
Mariana de O. Rigueiro <166918439+marianarigueiro@users.noreply.github.com>
2025-02-19 16:34:47 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dti
+ */
+public class Pessoa {
+    
+    String nome;
+    int idade;
+
+    public void definirNome(String valor) {
+        nome = valor;
+    }
+
+    public String retornarNome() {
+        return nome;
+    }
+
+    public void definirIdade(int valor) {
+        idade = valor;
+    }
+
+}
+
